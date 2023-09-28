aa5f51a928c4bd2d4ce22c24137fd87c4600ebad
Dimitri Rudolf <dimitri_rudolf@outlook.com>
2023-09-28 11:10:05 +0200

    Comment updates

--- src/main/kotlin/Boss.kt
+++ src/main/kotlin/Boss.kt
@@ -6,19 +6,13 @@ class Boss(
     name: String,
     var recource: Int
 ) : Enemy(hp, strg, int, agi, name) {
-    var newRecource = recource
     var empoweredSulfuras = false
     var abgetaucht = false
-    open fun isAlive(): Boolean {
-        return hp >= 1
-    }
-
-
     override fun enemyAction1(hero: Hero) { //Simple damage Action die einen damage over time Effekt hinterlässt
         val attackName = "Lava Burst"
         var lavaBurst = 1..5
         var dmg = (lavaBurst.random() * int)
-        newRecource += 15 //nach einsatz der Ability wird die Recource um 15 erhöht
+        recource += 15 //nach einsatz der Ability wird die Recource um 15 erhöht
         println("Ragnaros setzt $attackName ein und fügt damit ${hero.name} $dmg Schaden zu. Der Held erleidet jede Runde Schaden")
         hero.hp -= dmg
         hero.dotActive = true
@@ -28,7 +22,7 @@ class Boss(
         val attackName = "Blast Wave"
         var blastWave = 3..8
         var dmg = (blastWave.random() * int) / 2
-        newRecource += 10
+        recource += 10
         println("Ragnaros setzt $attackName ein und fügt damit ${hero.name} $dmg Schaden zu")
         hero.hp -= dmg
         if (dmg < 0){
@@ -42,14 +36,14 @@ class Boss(
         if (empoweredSulfuras == false) {
             empoweredSulfuras = true
             println("Ragnaros setzt $attackName ein und verstärkt Sulfuras mit Lava! Nächste Runde wirds weh tun!")
-            newRecource += 15
+            recource += 15
         }
         else if (empoweredSulfuras == true) { // Sollte empoweredSulfuras bereits true sein, so wird ein mächtiger Angriff ausgeführt
             var lavaSulfuras = 4..10
             var dmg = (lavaSulfuras.random() * strg) / 2 - hero.mDef
             println("Ich hab euch gewarnt!")
             println("Raganaros fügt ${hero.name}, $dmg Schaden zu")
-            newRecource += 15
+            recource += 15
             empoweredSulfuras = false // Setzt nach dem erfolgreichen Angriff empoweredSulfuras wieder auf false
             hero.hp -= dmg
             if (dmg < 0){
@@ -66,7 +60,7 @@ class Boss(
         var livingMeteor = 20..80
         var dmg = (livingMeteor.random() * 8) - hero.mDef
         println("Ragnaros setzt $attackName ein und fügt damit ${hero.name}, $dmg Schaden zu")
-        newRecource += 15
+        recource += 15
         hero.dotActive = true
         hero.hp -= dmg
         if (dmg < 0){
@@ -80,19 +74,19 @@ class Boss(
         if (abgetaucht == false) {
             println("Ragnaros saugt die Lava um Ihn herum auf")
             abgetaucht = true
-            newRecource += 25
+            recource += 25
         } else if (abgetaucht == true) { // Falls bereits abgetaucht, erhöht die Stats strg und int um jeweils 10
             println("Ragnaros entlädt seine Energie und erhöht seine Stärke und Initligenz um 100")
             abgetaucht = false // setzt abgetaucht wieder auf false
             strg += 100
             int += 100
-            newRecource += 25
+            recource += 25
         }
     }
 
     override fun enemyAction6(hero: Hero) { // Einsetztbar wenn Boss Recource 100 oder mehr erreicht. Verursacht viel schaden in einer großen range für alle Heros
         val attackName = "Wrath of Ragnaros"
-        if (newRecource >= 100) {
+        if (recource >= 100) {
             println("FEEL MY WRATH!")
             var wrathOfRagnaros = 3..10
             var dmg = (wrathOfRagnaros.random() * strg * int) / 10 - hero.pDef - hero.mDef
@@ -107,7 +101,7 @@ class Boss(
             val attackName = "Lava Burst"
             var lavaBurst = 1..5
             var dmg = (lavaBurst.random() * int) - hero.pDef + 100
-            newRecource += 15 //nach einsatz der Ability wird die Recource um 15 erhöht
+            recource += 15 //nach einsatz der Ability wird die Recource um 15 erhöht
             println("Ragnaros setzt $attackName ein und fügt damit ${hero.name} $dmg Schaden zu. Obwohl er einen Fehler gemacht hat?!")
             hero.hp -= dmg
             if (dmg < 0){
